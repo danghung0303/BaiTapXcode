e60b0fe6118023e4dc1b523b7802d988812e117b
danghung0303 <dangtanhung0303@gmail.com>
2023-04-19 22:37:54 +0700

    init folder BaitapXcode

---
+++ BaiTapXcode/BaiTapXcode/Bai1.swift
@@ -0,0 +1,19 @@
+//
+//  Bai1.swift
+//  BaiTapXcode
+//
+//  Created by Dang Hung on 19/04/2023.
+//
+
+import Foundation
+
+func bai1() {
+    
+    var x = 0
+    while x <= 0 {
+        print("Nhap x: ")
+        x = Int(readLine() ?? "") ?? 0
+    }
+    
+        print(x)
+}
---
+++ BaiTapXcode/BaiTapXcode/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  BaiTapXcode
+//
+//  Created by Dang Hung on 19/04/2023.
+//
+
+import Foundation
+
+
+func Bai1() {
+    
+}
+
+bai1()
